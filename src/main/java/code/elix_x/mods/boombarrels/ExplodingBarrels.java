0c4e54615e1cc0170e9831f3ca1b0c886f3f4c62
Elix_x <elix.x.ggl@gmail.com>
2020-02-28 00:20:38 +0100

    Deferred registering
    No need for dark sync magic

--- src/main/java/code/elix_x/mods/boombarrels/ExplodingBarrels.java
+++ src/main/java/code/elix_x/mods/boombarrels/ExplodingBarrels.java
@@ -1,24 +1,21 @@
 package code.elix_x.mods.boombarrels;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Semaphore;
-
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.BlockItem;
-import net.minecraft.util.ResourceLocation;
 
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.DeferredRegister;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -30,36 +27,28 @@ public class ExplodingBarrels {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 
-	public static final ResourceLocation BOOMBARREL = new ResourceLocation(MODID, "exploding_barrel");
-
-	public ExplodingBarrels(){
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-		MinecraftForge.EVENT_BUS.register(this);
-	}
-
-	public List<Block> blocks = new ArrayList<>();
-	public List<Item> items = new ArrayList<>();
-
 	/*
-	 * Initialization a-la once and for all (?)
-	 * Basically initializes once under any circumstance bypassing all possible multi-threadedness
+	 * Things static deferred registering
 	 */
 
-	private boolean initialized = false;
-	private final Semaphore initSemaphore = new Semaphore(1);
-	private static final int INITCALLSC = 3;
+	public static final String BOOMBARRELID = "exploding_barrel";
 
-	private void initialize(){
-		initSemaphore.acquire();
-		if(initialized) return;
-		//Actual Initialization
-		Block expBarrel = new ExplodingBarrelBlock(true, false).setRegistryName(BOOMBARREL);
-		blocks.add(expBarrel);
-		items.add(new BlockItem(expBarrel, new Item.Properties().group(ItemGroup.DECORATIONS)).setRegistryName(BOOMBARREL));
-		//Initialization complete
-		initialized = true;
-		initSemaphore.release(INITCALLSC - 1);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, MODID);
+	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, MODID);
+
+	public static final RegistryObject<ExplodingBarrelBlock> expBarrel = BLOCKS.reg(BOOMBARRELID, () -> new ExplodingBarrelBlock(true, false));
+	
+	static {
+		ITEMS.register(BOOMBARRELID, () -> new BlockItem(expBarrel, new Item.Properties().group(ItemGroup.DECORATIONS)));
+	}
+
+	public ExplodingBarrels(){
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modBus.addListener(this::setup);
+		modBus.addListener(this::clientSetup);
+		BLOCKS.register(modBus);
+		ITEMS.register(modBus);
+		MinecraftForge.EVENT_BUS.register(this);
 	}
 
 	/*
@@ -67,23 +56,11 @@ public class ExplodingBarrels {
 	 */
 
 	private void setup(final FMLCommonSetupEvent event){
-		initialize();
+		
 	}
 
 	private void clientSetup(final FMLClientSetupEvent event){
 		
-	}
-
-	@SubscribeEvent
-	public void registerBlocks(RegistryEvent.Register<Block> event){
-		initialize();
-		blocks.forEach(event.getRegistry()::register);
-	}
-
-	@SubscribeEvent
-	public void registerItems(RegistryEvent.Register<Item> event){
-		initialize();
-		items.forEach(event.getRegistry()::register);
 	}
 
 	public static class ExplodingBarrelBlock extends Block {
